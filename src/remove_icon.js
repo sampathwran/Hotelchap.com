84bcffb599f103019a1abf252a4940b5916d755f
Sampathwran <sampathwran@gmail.com>
2026-08-31 10:54:39 +0530

    Fix Vercel build by removing temp scripts and ignoring lint errors during build

--- src/remove_icon.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('C:/src/hotelchap-web/src/components/Header.tsx', 'utf8');
-
-content = content.replace('<button className="text-xl">🔍</button>', '');
-
-fs.writeFileSync('C:/src/hotelchap-web/src/components/Header.tsx', content, 'utf8');
-console.log('Fixed');
